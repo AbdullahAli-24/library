21f0b82fab6bb2dfd5e697b8fe175c353e1007d3
AbdullaAli-24 <moaob01142019787@gmail.com>
2026-08-04 23:51:34 +0300

    write models for loan app and views function

--- loan/urls.py
+++ loan/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('', views.loan,name='loan')
+urlpatterns = [
+    path( "", views.my_loans, name="my_loans"),
+    path( "borrow/<int:id>/", views.borrow_book, name="borrow_book" ),
+    path( "return/<int:id>/", views.return_book, name="return_book" ),
 ]
